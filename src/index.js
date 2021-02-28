96239ce54113e1878a3e5ee1c875b958cc54c396
uemuram <uemura.masaru1124@gmail.com>
2021-02-28 07:20:50 +0000

    問い合わせ用に改造前に一回コミット

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// 定数定義
+const cp = require("child_process");
+
 // for local debug
 let LOG = "";
 
@@ -10,22 +13,8 @@ function log(msg) {
     console.log(msg);
 }
 
-const cp = require("child_process");
-const exec = require('child_process').exec;
-const execSync = require('child_process').execSync;
-// const split = require('split');
 
-function execShellCommand(cmd) {
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.warn(error);
-            }
-            resolve(stdout ? stdout : stderr);
-        });
-    });
-}
-
+// ロジック類
 function createStdin() {
     //     return `usi
     // isready
@@ -43,14 +32,14 @@ function getResult(stdout) {
     //     bestpv: selectBestPv(infoList),
     //     info_list: infoList
     // })
+
     return new Promise(resolve => {
         // stdout.pipe(split()).on('data', data => {
         stdout.on('data', data => {
             const line = data.toString();
             const [cmd, ...words] = line.split(" ");
-            log(line);
-            log("abc");
-            log(cmd);
+            log(`[cmd: ${cmd}]`);
+            log(`[line: ${line}]`);
             // local_log(words);
             // if(cmd == "readyok") resolve("xyz2");
             resolve("xyz2");
@@ -62,30 +51,35 @@ function getResult(stdout) {
 
 exports.handler = async(event) => {
 
-    console.log('engine start');
+    log('engine start');
 
     log("a");
-    log("b");
 
     const proc = cp.spawn('./apery', [], { cwd: '/usr/local/apery/bin/' })
     let result;
-    //proc.stdin.write(createStdin());
-    // proc.stdin.write('usi\n');
-    // result = await getResult(proc.stdout)
+
+    log("b");
+
+    log(JSON.stringify(proc.stdout));
+
+    proc.stdin.write('usi\n');
+    result = await getResult(proc.stdout);
+
+    log("c");
 
     proc.stdin.write('isready\n');
-    result = await getResult(proc.stdout)
+    result = await getResult(proc.stdout);
 
+    log("d");
+
+    // proc.stdin.write('isready\n');
+    // result = await getResult(proc.stdout);
+
+    // log("e");
 
 
     proc.kill();
 
-
+    log('engine end');
     return LOG;
-
-
-
-
-
-
 };
