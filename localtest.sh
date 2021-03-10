0b83b6eeb0bc9110c89224d8144eaf74ce28b54d
uemuram <uemura.masaru1124@gmail.com>
2021-03-10 13:54:29 +0000

    テストコマンド整備

--- localtest.sh
+++ localtest.sh
@@ -2,8 +2,8 @@
 
 echo test
 
-docker cp src/.    c05:/usr/local/app/
-docker exec c05 npm install
-docker restart c05
+docker cp src/.    c07:/usr/local/app/
+docker exec c07 npm install
+docker restart c07
 
-curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"moves":["7g7f","8c8d"]}'
